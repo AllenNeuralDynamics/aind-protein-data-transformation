8c709ee56f52e27d620ff1ffe8ac5f361615ded4
Camilo Laiton <36769694+camilolaiton@users.noreply.github.com>
2025-03-03 11:42:02 -0800

    adding job to upload tiles from czi to zarr

---
+++ src/aind_protein_data_transformation/utils/utils.py
@@ -0,0 +1,201 @@
+"""
+Utility functions for image readers
+"""
+
+import json
+import os
+import platform
+import subprocess
+from typing import Optional
+
+import numpy as np
+
+from aind_protein_data_transformation.models import ArrayLike, PathLike
+
+
+def add_leading_dim(data):
+    """
+    Adds a new dimension to existing data.
+    Parameters
+    ------------------------
+    arr: ArrayLike
+        Dask/numpy array that contains image data.
+
+    Returns
+    ------------------------
+    ArrayLike:
+        Padded dask/numpy array.
+    """
+
+    return data[None, ...]
+
+
+def pad_array_n_d(arr, dim: int = 5):
+    """
+    Pads a daks array to be in a 5D shape.
+
+    Parameters
+    ------------------------
+
+    arr: ArrayLike
+        Dask/numpy array that contains image data.
+    dim: int
+        Number of dimensions that the array will be padded
+
+    Returns
+    ------------------------
+    ArrayLike:
+        Padded dask/numpy array.
+    """
+    if dim > 5:
+        raise ValueError("Padding more than 5 dimensions is not supported.")
+
+    while arr.ndim < dim:
+        arr = arr[np.newaxis, ...]
+    return arr
+
+
+def extract_data(
+    arr, last_dimensions: Optional[int] = None
+):
+    """
+    Extracts n dimensional data (numpy array or dask array)
+    given expanded dimensions.
+    e.g., (1, 1, 1, 1600, 2000) -> (1600, 2000)
+    e.g., (1, 1600, 2000) -> (1600, 2000)
+    e.g., (1, 1, 2, 1600, 2000) -> (2, 1600, 2000)
+
+    Parameters
+    ------------------------
+    arr: ArrayLike
+        Numpy or dask array with image data. It is assumed
+        that the last dimensions of the array contain
+        the information about the image.
+
+    last_dimensions: Optional[int]
+        If given, it selects the number of dimensions given
+        stating from the end
+        of the array
+        e.g., arr=(1, 1, 1600, 2000) last_dimensions=3 -> (1, 1600, 2000)
+        e.g., arr=(1, 1, 1600, 2000) last_dimensions=1 -> (2000)
+
+    Raises
+    ------------------------
+    ValueError:
+        Whenever the last dimensions value is higher
+        than the array dimensions.
+
+    Returns
+    ------------------------
+    ArrayLike:
+        Reshaped array with the selected indices.
+    """
+
+    if last_dimensions is not None:
+        if last_dimensions > arr.ndim:
+            raise ValueError(
+                "Last dimensions should be lower than array dimensions"
+            )
+
+    else:
+        last_dimensions = len(arr.shape) - arr.shape.count(1)
+
+    dynamic_indices = [slice(None)] * arr.ndim
+
+    for idx in range(arr.ndim - last_dimensions):
+        dynamic_indices[idx] = 0
+
+    return arr[tuple(dynamic_indices)]
+
+
+def read_json_as_dict(filepath: PathLike) -> dict:
+    """
+    Reads a json as dictionary.
+
+    Parameters
+    ------------------------
+
+    filepath: PathLike
+        Path where the json is located.
+
+    Returns
+    ------------------------
+
+    dict:
+        Dictionary with the data the json has.
+
+    """
+
+    dictionary = {}
+
+    if os.path.exists(filepath):
+        with open(filepath) as json_file:
+            dictionary = json.load(json_file)
+
+    return dictionary
+
+
+def sync_dir_to_s3(directory_to_upload: PathLike, s3_location: str) -> None:
+    """
+    Syncs a local directory to an s3 location by running aws cli in a
+    subprocess.
+
+    Parameters
+    ----------
+    directory_to_upload : PathLike
+    s3_location : str
+
+    Returns
+    -------
+    None
+
+    """
+    # Upload to s3
+    if platform.system() == "Windows":
+        shell = True
+    else:
+        shell = False
+
+    base_command = [
+        "aws",
+        "s3",
+        "sync",
+        str(directory_to_upload),
+        s3_location,
+        "--only-show-errors",
+    ]
+
+    subprocess.run(base_command, shell=shell, check=True)
+
+
+def copy_file_to_s3(file_to_upload: PathLike, s3_location: str) -> None:
+    """
+    Syncs a local directory to an s3 location by running aws cli in a
+    subprocess.
+
+    Parameters
+    ----------
+    file_to_upload : PathLike
+    s3_location : str
+
+    Returns
+    -------
+    None
+
+    """
+    # Upload to s3
+    if platform.system() == "Windows":
+        shell = True
+    else:
+        shell = False
+
+    base_command = [
+        "aws",
+        "s3",
+        "cp",
+        str(file_to_upload),
+        s3_location,
+        "--only-show-errors",
+    ]
+
+    subprocess.run(base_command, shell=shell, check=True)
--- src/aind_protein_data_transformation/zeiss_job.py
+++ src/aind_protein_data_transformation/zeiss_job.py
@@ -18,7 +18,228 @@ from aind_protein_data_transformation.compress.czi_to_zarr import (
 )
 from aind_protein_data_transformation.models import (
     CompressorName,
-    SmartspimJobSettings,
+    ZeissJobSettings,
 )
+import bioio_czi
+from bioio import BioImage
+import dask.array as da
+
+import re
+
+from aind_protein_data_transformation.utils import utils
 
 logging.basicConfig(level=os.getenv("LOG_LEVEL", "WARNING"))
+
+class ZeissCompressionJob(GenericEtl[ZeissJobSettings]):
+    """Job to handle compressing and uploading Zeiss data."""
+
+    def _get_partitioned_list_of_stack_paths(self) -> List[List[Path]]:
+        """
+        Scans through the input source and partitions a list of stack
+        paths that it finds there.
+        """
+        all_stack_paths = []
+        total_counter = 0
+        for p in Path(self.job_settings.input_source).glob("*.czi"):
+            if p.is_file():
+                total_counter += 1
+                all_stack_paths.append(p)
+        
+        # Important to sort paths so every node computes the same list
+        all_stack_paths.sort(key=lambda x: str(x))
+        return all_stack_paths
+
+    @staticmethod
+    def _get_voxel_resolution(acquisition_path: Path) -> List[float]:
+        """Get the voxel resolution from an acquisition.json file."""
+
+        if not acquisition_path.is_file():
+            raise FileNotFoundError(
+                f"acquisition.json file not found at: {acquisition_path}"
+            )
+
+        acquisition_config = utils.read_json_as_dict(acquisition_path)
+
+        # Grabbing a tile with metadata from acquisition - we assume all
+        # dataset was acquired with the same resolution
+        tile_coord_transforms = acquisition_config["tiles"][0][
+            "coordinate_transformations"
+        ]
+
+        scale_transform = [
+            x["scale"] for x in tile_coord_transforms if x["type"] == "scale"
+        ][0]
+
+        x = float(scale_transform[0])
+        y = float(scale_transform[1])
+        z = float(scale_transform[2])
+
+        return [z, y, x]
+
+    def _get_compressor(self) -> Optional[Blosc]:
+        """
+        Utility method to construct a compressor class.
+        Returns
+        -------
+        Blosc | None
+          An instantiated Blosc compressor. Return None if not set in configs.
+
+        """
+        if self.job_settings.compressor_name == CompressorName.BLOSC:
+            return Blosc(**self.job_settings.compressor_kwargs)
+        else:
+            return None
+
+    def _write_stacks(self, stacks_to_process: List) -> None:
+        """
+        Write a list of stacks.
+        Parameters
+        ----------
+        stacks_to_process : List
+
+        Returns
+        -------
+        None
+
+        """
+        compressor = self._get_compressor()
+        # acquisition_path = Path(self.job_settings.input_source).joinpath(
+        #     "acquisition.json"
+        # )
+        # voxel_size_zyx = self._get_voxel_resolution(
+        #     acquisition_path=acquisition_path
+        # )
+        # logging.info(
+        #     f"Stacks to process: {stacks_to_process}, - Voxel res: "
+        #     f"{voxel_size_zyx}"
+        # )
+
+        for stack in stacks_to_process:
+            logging.info(f"Converting {stack}")
+            stack_name = stack.name
+            match = re.match(r"(.+)\((\d+)\)\.czi", stack_name)
+
+            if match:
+                base_name, number = match.groups()
+                stack_name = f"{base_name}_{number}"
+
+            else:
+                raise ValueError(f"Stack name was not able to be parsed: {stack}")
+
+            output_path = Path(self.job_settings.output_directory).joinpath(
+                stack_name
+            )
+
+            czi_file_reader = BioImage(
+                str(stack), reader=bioio_czi.Reader
+            )
+
+            voxel_size_zyx = czi_file_reader.physical_pixel_sizes
+
+            delayed_stack = da.squeeze(czi_file_reader.dask_data)
+
+            czi_stack_zarr_writer(
+                image_data=delayed_stack,
+                output_path=output_path,
+                voxel_size=voxel_size_zyx,
+                final_chunksize=self.job_settings.chunk_size,
+                scale_factor=self.job_settings.scale_factor,
+                n_lvls=self.job_settings.downsample_levels,
+                channel_name=stack_name,
+                stack_name=f"{stack_name}.ome.zarr",
+                logger=logging,
+                writing_options=compressor,
+            )
+
+            if self.job_settings.s3_location is not None:
+                channel_zgroup_file = output_path / ".zgroup"
+                s3_channel_zgroup_file = (
+                    f"{self.job_settings.s3_location}/{stack_name}/.zgroup"
+                )
+                logging.info(
+                    f"Uploading {channel_zgroup_file} to "
+                    f"{s3_channel_zgroup_file}"
+                )
+                utils.copy_file_to_s3(
+                    channel_zgroup_file, s3_channel_zgroup_file
+                )
+                ome_zarr_stack_name = f"{stack_name}.ome.zarr"
+                ome_zarr_stack_path = output_path.joinpath(ome_zarr_stack_name)
+                s3_stack_dir = (
+                    f"{self.job_settings.s3_location}/{stack_name}/"
+                    f"{ome_zarr_stack_name}"
+                )
+                logging.info(
+                    f"Uploading {ome_zarr_stack_path} to {s3_stack_dir}"
+                )
+                utils.sync_dir_to_s3(ome_zarr_stack_path, s3_stack_dir)
+                logging.info(f"Removing: {ome_zarr_stack_path}")
+                # Remove stack if uploaded to s3. We can potentially do all
+                # the stacks in the partition in parallel using dask to speed
+                # this up
+                shutil.rmtree(ome_zarr_stack_path)
+
+    def _upload_derivatives_folder(self):
+        """
+        Uploads the derivatives folder inside of
+        the SPIM folder in the cloud.
+        """
+        s3_derivatives_dir = f"{self.job_settings.s3_location}/derivatives"
+        derivatives_path = Path(self.job_settings.input_source).joinpath(
+            "derivatives"
+        )
+
+        if not derivatives_path.exists():
+            raise FileNotFoundError(f"{derivatives_path} does not exist.")
+
+        if self.job_settings.s3_location is not None:
+            logging.info(
+                f"Uploading {derivatives_path} to {s3_derivatives_dir}"
+            )
+            utils.sync_dir_to_s3(derivatives_path, s3_derivatives_dir)
+            logging.info(f"{derivatives_path} uploaded to s3.")
+
+    def run_job(self):
+        """Main entrypoint to run the job."""
+        job_start_time = time()
+
+        partitioned_list = self._get_partitioned_list_of_stack_paths()
+
+        # Upload derivatives folder
+        # if self.job_settings.partition_to_process == 0:
+        #     self._upload_derivatives_folder()
+
+        stacks_to_process = partitioned_list[
+            self.job_settings.partition_to_process
+        ]
+
+        self._write_stacks(stacks_to_process=stacks_to_process)
+        total_job_duration = time() - job_start_time
+        return JobResponse(
+            status_code=200, message=f"Job finished in {total_job_duration}"
+        )
+
+
+# TODO: Add this to core aind_data_transformation class
+def job_entrypoint(sys_args: list):
+    """Main function"""
+    parser = get_parser()
+    cli_args = parser.parse_args(sys_args)
+    if cli_args.job_settings is not None:
+        job_settings = ZeissJobSettings.model_validate_json(
+            cli_args.job_settings
+        )
+    elif cli_args.config_file is not None:
+        job_settings = ZeissJobSettings.from_config_file(
+            cli_args.config_file
+        )
+    else:
+        # Construct settings from env vars
+        job_settings = ZeissJobSettings()
+    job = ZeissJobSettings(job_settings=job_settings)
+    job_response = job.run_job()
+    logging.info(job_response.model_dump_json())
+
+
+if __name__ == "__main__":
+    job_entrypoint(sys.argv[1:])
