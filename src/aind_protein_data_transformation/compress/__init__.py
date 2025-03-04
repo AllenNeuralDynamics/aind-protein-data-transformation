e4acdd2f8ee1d8691d576c4279c5980f0b73b523
Camilo Laiton <36769694+camilolaiton@users.noreply.github.com>
2025-03-03 16:41:27 -0800

    adding license

--- src/aind_protein_data_transformation/compress/__init__.py
+++ src/aind_protein_data_transformation/compress/__init__.py
@@ -1,3 +1,20 @@
 """
 CZI to Zarr stack conversion
 """
+__version__ = "0.0.1"
+__authors__ = ["Camilo Laiton"]
+__author_emails__ = [
+    "camilo.laiton@alleninstitute.org",
+]
+__license__ = "MIT"
+__description__ = (
+    "Repository for transforming CZI data to ome.zarr"
+)
+__url__ = "https://github.com/AllenNeuralDynamics/aind-protein-data-transformation"
+
+__maintainers__ = ["Camilo Laiton"]
+__maintainer_emails__ = [
+    "camilo.laiton@alleninstitute.org",
+]
+__title__ = "aind-protein-data-transformation"
+__status__ = "Development"  # 'Development' 'Production', 'Beta'
