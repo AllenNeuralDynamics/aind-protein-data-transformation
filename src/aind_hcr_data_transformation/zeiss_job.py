a4702dda323e1640813ccbcf1dba493e43908e59
Carson Berry <carson.berry@carsonb-ux1.corp.alleninstitute.org>
2025-08-29 09:41:45 -0700

    change log to debug

--- src/aind_hcr_data_transformation/zeiss_job.py
+++ src/aind_hcr_data_transformation/zeiss_job.py
@@ -21,7 +21,7 @@ from aind_hcr_data_transformation.models import (
 )
 from aind_hcr_data_transformation.utils import utils
 
-logging.basicConfig(level=os.getenv("LOG_LEVEL", "INFO"))
+logging.basicConfig(level=os.getenv("LOG_LEVEL", "DEBUG"))
 
 
 class ZeissCompressionJob(GenericEtl[ZeissJobSettings]):
