0fc3752de5cc78830b29e976e677bf42bf8677a2
cbrrrry <carson.berry@alleninstitute.org>
2026-06-08 11:40:32 -0700

    add back aws version

--- src/aind_hcr_data_transformation/__init__.py
+++ src/aind_hcr_data_transformation/__init__.py
@@ -2,18 +2,19 @@
 CZI to Zarr stack conversion
 """
 
-__version__ = "0.0.7"
-__authors__ = ["Camilo Laiton"]
+__version__ = "0.0.8"
+__authors__ = ["Camilo Laiton", "Carson Berry"]
 __author_emails__ = [
     "camilo.laiton@alleninstitute.org",
+    "carson.berry@alleninstitute.org",
 ]
 __license__ = "MIT"
 __description__ = "Repository for transforming CZI data to zarr"
 __url__ = "https://github.com/AllenNeuralDynamics/aind-hcr-data-transformation"
 
-__maintainers__ = ["Camilo Laiton"]
+__maintainers__ = ["Carson Berry"]
 __maintainer_emails__ = [
-    "camilo.laiton@alleninstitute.org",
+    "carson.berry@alleninstitute.org",
 ]
 __title__ = "aind-hcr-data-transformation"
 __status__ = "Production"  # 'Development' 'Production', 'Beta'
